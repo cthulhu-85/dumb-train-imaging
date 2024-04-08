9a8f811688cab0842a85a51f6454269139441617
Julian Ward <julianward07@gmail.com>
2024-04-08 08:12:13 -0700

    Updated poetry.lock, and aparently did a bunch of other things.

--- lightdetect/__init__.py
+++ lightdetect/__init__.py
@@ -2,7 +2,29 @@
 # import the opencv library 
 import cv2 
 import os
- 
+import time
+
+# From chatgpt
+class Timer:
+    def __init__(self, duration):
+        self.duration = duration
+        self.start_time = None
+
+    def start(self):
+        self.start_time = time.time()
+
+    def is_done(self):
+        if self.start_time is None:
+            return False
+        elapsed_time = time.time() - self.start_time
+        return elapsed_time >= self.duration
+
+    def is_started(self):
+        return not (self.start_time == None)
+
+    def reset(self):
+        self.start_time = None
+camera = input("wich camera?!") 
 # define a video capture object 
 vid = cv2.VideoCapture(1) 
 
@@ -13,11 +35,22 @@ how_many_times_the_train_aint_came = 0
 has_played = False
 
 
+MAX_FRAMES = 121
+current_frame = 1
+
+
+train_came_timer = Timer(5)
   
 while(True): 
     # Capture the video frame 
     # by frame 
     ret, frame = vid.read() 
+
+    current_frame = 1+(current_frame+30)%MAX_FRAMES
+    print("current_frame=", current_frame)
+
+    tmp_image = cv2.imread("./rail_crossing/"+str(current_frame)+".png")
+
 
     print("shape=", frame.shape)
 
@@ -45,7 +78,12 @@ while(True):
 
     # Display the resulting frame 
     cv2.imshow('frame', frame) 
-     
+
+    if train_came or not train_came_timer.is_done():
+        cv2.imshow('frame', tmp_image)
+        if not train_came_timer.is_started():
+            train_came_timer = Timer(5)
+            train_came_timer.start()
     # the 'q' button is set as the 
     # quitting button you may use any 
     # desired button of your choice 
